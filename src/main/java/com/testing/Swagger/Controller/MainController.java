0534b9b48d34fcf1bf3225c35e55015b218cdbef
MahmoudBdran <41477881+MahmoudBdran@users.noreply.github.com>
2024-04-28 01:42:08 +0300

    unwanted edit Please rollback2

--- src/main/java/com/testing/Swagger/Controller/MainController.java
+++ src/main/java/com/testing/Swagger/Controller/MainController.java
@@ -11,4 +11,8 @@ public class MainController {
     public String index() {
         return "Hurraaaaaay!! it is working";
     }
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hurraaaaaay!! it is working";
+    }
 }
