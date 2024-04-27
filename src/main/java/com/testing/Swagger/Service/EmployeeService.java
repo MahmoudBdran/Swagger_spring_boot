911ae06bc344ab569e5fd3e013d569bdec565a17
MahmoudBdran <41477881+MahmoudBdran@users.noreply.github.com>
2024-04-28 01:28:21 +0300

    unwanted edit Please rollback

--- src/main/java/com/testing/Swagger/Service/EmployeeService.java
+++ src/main/java/com/testing/Swagger/Service/EmployeeService.java
@@ -1,28 +1,28 @@
-package com.testing.Swagger.Service;
-
-
-import com.testing.Swagger.Entity.Employee;
-import com.testing.Swagger.Repository.EmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
-
-@Service
-public class EmployeeService {
-    @Autowired
-    EmployeeRepository employeeRepository;
-
-    public List<Employee> getAllEmployees(){
-        return employeeRepository.findAll();
-    }
-    public Optional<Employee> getEmployeeById(Long id){
-        return employeeRepository.findById(id);
-    }
-    public Employee InsertEmployee (Employee employee){
-        return employeeRepository.save(employee);
-    }
-
-
-}
+//package com.testing.Swagger.Service;
+//
+//
+//import com.testing.Swagger.Entity.Employee;
+//import com.testing.Swagger.Repository.EmployeeRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Service
+//public class EmployeeService {
+//    @Autowired
+//    EmployeeRepository employeeRepository;
+//
+//    public List<Employee> getAllEmployees(){
+//        return employeeRepository.findAll();
+//    }
+//    public Optional<Employee> getEmployeeById(Long id){
+//        return employeeRepository.findById(id);
+//    }
+//    public Employee InsertEmployee (Employee employee){
+//        return employeeRepository.save(employee);
+//    }
+//
+//
+//}
